a41df9bdb2cffa165e6b2bbf56205acf4c671c31
chaan32 <haechan8864@naver.com>
2025-03-20 22:04:28 +0900

    1주차 백엔드 코드

---
+++ chatserver/src/main/java/com/example/chatserver/common/configs/Securityconfigs.java
@@ -0,0 +1,57 @@
+package com.example.chatserver.common.configs;
+
+import com.example.chatserver.common.auth.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@RequiredArgsConstructor
+public class Securityconfigs {
+    private final JwtAuthFilter jwtAuthFilter;
+  
+    @Bean
+    public SecurityFilterChain myFilter(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors(cors->cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable) // 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable) // 비활성화
+                // 특정 URL 패턴에 대해서는 인증 처리를 하지 않을 것
+                .authorizeHttpRequests(a -> a.requestMatchers("/member/create", "/member/doLogin", "/connect/**").permitAll().anyRequest().authenticated())
+                // 세션을 사용하지 않을 것
+                .sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // 필터 등록
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("*")); //모든 HTTP메서드 허용
+        configuration.setAllowedHeaders(Arrays.asList("*")); //모든 헤더값 허용
+        configuration.setAllowCredentials(true); //자격증명허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); //모든 url에 패턴에 대해 cors 허용 설정
+        return source;
+    }
+
+    @Bean
+    public PasswordEncoder makePassword(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
